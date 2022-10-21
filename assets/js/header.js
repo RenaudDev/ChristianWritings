7ce8b296f5b40a28bf0318b4f96eea4fcb2fe060
Renaud <renaudgagne@outlook.com>
2022-10-21 19:06:29 -0400

    Try improve PWA setup

--- assets/js/header.js
+++ assets/js/header.js
@@ -54,13 +54,3 @@ window.addEventListener("DOMContentLoaded", function () {
         dark_mode_btn.classList.remove('hidden');
     });
 });
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js');
-  }
-  
-  document.querySelector('#show').addEventListener('click', () => {
-    const iconUrl = document.querySelector('select').selectedOptions[0].value;
-    let imgElement = document.createElement('img');
-    imgElement.src = iconUrl;
-    document.querySelector('#container').appendChild(imgElement);
-  });
