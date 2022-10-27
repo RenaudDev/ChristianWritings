93a7a52f449dc5bcb305c79c0b051f47cd211b7e
Renaud <renaudgagne@outlook.com>
2022-10-27 09:03:47 -0400

    Try service worker strategy

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,12 @@
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
-workbox.routing.registerRoute(
+/* workbox.routing.registerRoute(
   ({request}) => request.destination === '/',
   new workbox.strategies.CacheFirst()
+);
+*/
+workbox.routing.registerRoute(
+  /\.(?:js|css|webp|png|svg)$/,
+  new workbox.strategies.StaleWhileRevalidate()
 );
