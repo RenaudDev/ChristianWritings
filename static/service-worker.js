2616485c589894d97869884263033fe923137e31
Renaud <renaudgagne@outlook.com>
2022-10-28 12:07:50 -0400

    rework service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -7,20 +7,14 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 workbox.routing.registerRoute(
-  /\.(?:js|css)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'static-resources',
-  })
-);
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images',
+  /\.(?:webp|png|svg|ico)$/,
+  new workbox.strategies.CacheFirst({
+    "cacheName": "images",
     plugins: [
       new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
+        maxEntries: 1000,
+        maxAgeSeconds: 31536000
+      })
+    ]
   })
 );
