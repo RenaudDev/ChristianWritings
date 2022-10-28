05c41a0ac878218c91ecc82339c4ed6e78f2df22
Renaud <renaudgagne@outlook.com>
2022-10-28 10:43:03 -0400

    new service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -6,18 +6,27 @@
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
-
-
 workbox.routing.registerRoute(
-  /\.(?:css|js)$/,
+  /\.(?:js|css)$/,
   new workbox.strategies.StaleWhileRevalidate({
     "cacheName": "assets",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 604800
+      })
+    ]
   })
 );
-
 workbox.routing.registerRoute(
   /\.(?:webp|png|svg|ico)$/,
   new workbox.strategies.CacheFirst({
     "cacheName": "images",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 31536000
+      })
+    ]
   })
 );
