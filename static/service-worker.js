39499770acba4228ae2a4237789d65fcfee3d2e9
Renaud <renaudgagne@outlook.com>
2022-10-28 13:48:41 -0400

    Audio + Fallback Service Worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,11 @@
 import { registerRoute, Route } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 import { ExpirationPlugin } from 'workbox-expiration';
+import { offlineFallback } from 'workbox-recipes';
+import { setDefaultHandler } from 'workbox-routing';
+import { NetworkOnly}  from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { RangeRequestsPlugin } from 'workbox-range-requests';
 
 // Evict image cache entries older thirty days:
 const imageRoute = new Route(({ request }) => {
@@ -30,3 +35,27 @@ const scriptsRoute = new Route(({ request }) => {
 // Register routes
 registerRoute(imageRoute);
 registerRoute(scriptsRoute);
+
+// sw.js
+
+
+registerRoute(
+  ({request}) => {
+    const {destination} = request;
+
+    destination === 'audio'
+  },
+  new CacheFirst({
+    cacheName: 'audioCache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [200]
+      }),
+      new RangeRequestsPlugin(),
+    ],
+  }),
+);
+
+setDefaultHandler(new NetworkOnly());
+
+offlineFallback();
