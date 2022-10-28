651cc72679e7cb6c034afd224f1f20fbd8182ecc
Renaud <renaudgagne@outlook.com>
2022-10-28 13:53:02 -0400

    Workers

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,11 +1,6 @@
 import { registerRoute, Route } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { offlineFallback } from 'workbox-recipes';
-import { setDefaultHandler } from 'workbox-routing';
-import { NetworkOnly}  from 'workbox-strategies';
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { RangeRequestsPlugin } from 'workbox-range-requests';
 
 // Evict image cache entries older thirty days:
 const imageRoute = new Route(({ request }) => {
@@ -37,7 +32,10 @@ registerRoute(imageRoute);
 registerRoute(scriptsRoute);
 
 // sw.js
-
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+import {RangeRequestsPlugin} from 'workbox-range-requests';
 
 registerRoute(
   ({request}) => {
@@ -55,7 +53,3 @@ registerRoute(
     ],
   }),
 );
-
-setDefaultHandler(new NetworkOnly());
-
-offlineFallback();
