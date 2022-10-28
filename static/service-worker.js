f46b2577c9ffb4e34c9aa8988236b595fe2a6074
Renaud <renaudgagne@outlook.com>
2022-10-28 14:22:01 -0400

    Try fallback

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,37 +1,98 @@
-import {warmStrategyCache} from 'workbox-recipes';
-import {setDefaultHandler, setCatchHandler} from 'workbox-routing';
-import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies';
 
-// Fallback assets to cache
-const FALLBACK_HTML_URL = '/offline.html';
-const FALLBACK_IMAGE_URL = '/offline.png';
-const FALLBACK_STRATEGY = new CacheFirst();
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
 
-// Warm the runtime cache with a list of asset URLs
-warmStrategyCache({
-  urls: [FALLBACK_HTML_URL, FALLBACK_IMAGE_URL],
-  strategy: FALLBACK_STRATEGY,
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+/*
+Copyright 2015, 2019, 2020, 2021 Google LLC. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
+// This variable is intentionally declared and unused.
+// Add a comment for your linter if you want:
+// eslint-disable-next-line no-unused-vars
+
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+// Customize this with a different URL if needed.
+const OFFLINE_URL = "offline.html";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      // Setting {cache: 'reload'} in the new request ensures that the
+      // response isn't fulfilled from the HTTP cache; i.e., it will be
+      // from the network.
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
+  );
+  // Force the waiting service worker to become the active service worker.
+  self.skipWaiting();
 });
 
-// Use a stale-while-revalidate strategy to handle requests by default.
-setDefaultHandler(new StaleWhileRevalidate());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
 
-// This "catch" handler is triggered when any of the other routes fail to
-// generate a response.
-setCatchHandler(async ({request}) => {
-  // The warmStrategyCache recipe is used to add the fallback assets ahead of
-  // time to the runtime cache, and are served in the event of an error below.
-  // Use `event`, `request`, and `url` to figure out how to respond, or
-  // use request.destination to match requests for specific resource types.
-  switch (request.destination) {
-    case 'document':
-      return FALLBACK_STRATEGY.handle({event, request: FALLBACK_HTML_URL});
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
 
-    case 'image':
-      return FALLBACK_STRATEGY.handle({event, request: FALLBACK_IMAGE_URL});
+self.addEventListener("fetch", (event) => {
+  // Only call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's
+          // supported.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
 
-    default:
-      // If we don't have a fallback, return an error response.
-      return Response.error();
+          // Always try the network first.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is
+          // likely due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
   }
+
+  // If our if() condition is false, then this fetch handler won't
+  // intercept the request. If there are any other fetch handlers
+  // registered, they will get a chance to call event.respondWith().
+  // If no fetch handlers call event.respondWith(), the request
+  // will be handled by the browser as if there were no service
+  // worker involvement.
 });
