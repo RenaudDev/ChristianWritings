b1c2251b5ee32165585e9794f8dd9384130bcb33
Renaud <renaudgagne@outlook.com>
2022-10-29 13:35:23 -0400

    Try something else

--- static/sw.ts
+++ static/sw.ts
@@ -1,7 +1,4 @@
 import {precacheAndRoute} from 'workbox-precaching';
-import {offlineFallback} from 'workbox-recipes';
-import {setDefaultHandler} from 'workbox-routing';
-import {NetworkOnly} from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
