0e90d52e8f523921f149b0708346bf9b977f6bda
Renaud <renaudgagne@outlook.com>
2022-10-29 16:38:21 -0400

    try something else

--- static/sw.ts
+++ static/sw.ts
@@ -1,12 +1,29 @@
 import {precacheAndRoute} from 'workbox-precaching';
-import {offlineFallback} from 'workbox-recipes';
-import {setDefaultHandler} from 'workbox-routing';
-import {NetworkOnly} from 'workbox-strategies';
+import {matchPrecache, precacheAndRoute} from 'workbox-precaching';
+import {setDefaultHandler, setCatchHandler} from 'workbox-routing';
+import {StaleWhileRevalidate} from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
+const FALLBACK_HTML_URL = '/offline.html';
+const FALLBACK_IMAGE_URL = '/offline.png';
+
 precacheAndRoute(self.__WB_MANIFEST);
 
-setDefaultHandler(new NetworkOnly());
+setDefaultHandler(new StaleWhileRevalidate());
 
-offlineFallback();
+setCatchHandler(async ({request}) => {
+    switch (request.destination) {
+        case 'document':
+          // FALLBACK_HTML_URL must be defined as a precached URL for this to work:
+          return matchPrecache(FALLBACK_HTML_URL);
+    
+        case 'image':
+          // FALLBACK_IMAGE_URL must be defined as a precached URL for this to work:
+          return matchPrecache(FALLBACK_IMAGE_URL);
+    
+        default:
+          // If we don't have a fallback, return an error response.
+          return Response.error();
+      }
+    });
