04209b927e9b5cd4ed0a91707433313f3b2f7c50
Renaud <renaudgagne@outlook.com>
2022-10-29 13:54:11 -0400

    Try something

--- static/workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	globDirectory: '/public',
-	globPatterns: [
-		'**/*.{js,json,css,scss,webmanifest}'
-	],
-	swDest: '/static/sw.js',
-	swSrc: '/static/sw.js'
-};
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	globDirectory: 'public/',
+	globPatterns: [
+		'**/*.{js,css,scss}'
+	],
+	swDest: 'public/sw.js',
+	swSrc: 'static/sw.js'
+};
