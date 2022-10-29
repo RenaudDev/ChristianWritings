036f516e004130d1349db0ff9a12e788d15f560b
Renaud <renaudgagne@outlook.com>
2022-10-29 13:29:47 -0400

    Update SW

--- static/sw.ts
+++ static/sw.ts
@@ -1,12 +1,12 @@
 import {precacheAndRoute} from 'workbox-precaching';
 import {offlineFallback} from 'workbox-recipes';
 import {setDefaultHandler} from 'workbox-routing';
-import {NetworkOnly} from 'workbox-strategies';
+import {StaleWhileRevalidate} from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-setDefaultHandler(new NetworkOnly());
+setDefaultHandler(new StaleWhileRevalidate());
 
 offlineFallback();
