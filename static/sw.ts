5796a1c280752c337f5ec9fb3cb8350518342c35
Renaud <renaudgagne@outlook.com>
2022-10-30 08:49:05 -0400

    try

--- static/sw.ts
+++ static/sw.ts
@@ -1,7 +1,20 @@
 import {precacheAndRoute} from 'workbox-precaching';
-import {imageCache} from 'workbox-recipes';
+import { imageCache } from 'workbox-recipes';
+import { googleFontsCache } from 'workbox-recipes';
+import { pageCache } from 'workbox-recipes';
+import { offlineFallback } from 'workbox-recipes';
+import { setDefaultHandler } from 'workbox-routing';
+import { NetworkOnly } from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+setDefaultHandler(new NetworkOnly());
+
+offlineFallback();
+
 imageCache();
+googleFontsCache();
+pageCache();
+
