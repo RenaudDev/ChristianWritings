f788bea8582951dc8769ab8f2e0447d71ad48d69
Renaud <renaudgagne@outlook.com>
2022-10-30 08:27:10 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -2,13 +2,6 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-import { imageCache } from 'workbox-recipes';
-import { googleFontsCache } from 'workbox-recipes';
-import { pageCache } from 'workbox-recipes';
-import { offlineFallback } from 'workbox-recipes';
-import { setDefaultHandler } from 'workbox-routing';
-import { NetworkOnly } from 'workbox-strategies';
-
 setDefaultHandler(new NetworkOnly());
 
 offlineFallback();
