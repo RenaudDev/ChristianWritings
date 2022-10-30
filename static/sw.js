90216bdabd58825c353c9f389abffcd1411cdac2
Renaud <renaudgagne@outlook.com>
2022-10-30 08:23:41 -0400

    try again

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,7 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
 import { imageCache } from 'workbox-recipes';
 import { googleFontsCache } from 'workbox-recipes';
 import { pageCache } from 'workbox-recipes';
